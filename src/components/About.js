abfdd36fbb06a2377718d49f8dcf2ec10936ca85
abid0801 <abid.hassan@northsouth.edu>
2021-04-16 01:08:18 +0600

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+const About = () => {
+  return (
+  <div>
+    <h4>Version 14.04.2021</h4>
+    <p>1st react project from Shehri to iftar. Thanks to Traversy Media. </p>
+    <Link to='/'>Go Back</Link>
+  </div>
+  )
+}
+export default About
